87ba18cef011958a8d2dd89501e4cf2f47ea1df1
MercerMahaffey <89871123+MercerMahaffey@users.noreply.github.com>
2021-11-04 17:22:13 -0400

    authentication

--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
+
+
+const cookieSession = require('cookie-session');
+
 
 router.get('/login', (req, res) => {
     res.render('login')
 })
+
+router.post('/login', passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login'
+}))
 
 
 
