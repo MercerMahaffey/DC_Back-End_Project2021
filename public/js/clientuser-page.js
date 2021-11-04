eb416617259284c54721227131f6e7b986e87086
James Ivy <jameskivy@gmail.com>
2021-11-04 13:19:48 -0500

    front-end

--- public/js/clientuser-page.js
+++ public/js/clientuser-page.js
@@ -1,7 +1,31 @@
 //in public --> js
 
-let inputPost = document.querySelector('inputCreate')
+let form = document.querySelector('form')
 
-let users = (params) => {
+form.addEventListener('submit', async (e) => {
+    e.preventDefault()
+})
 
+//make a fetch
+
+let newPost = {
+    title: document.querySelector('#post-title').value,
+    message: document.querySelector('#post-message').value
 }
+
+let results = await fetch('/', {
+    method: "POST",
+
+})
+
+let messages = await results.json()
+updateStatus(messages)
+
+
+
+//grab the data all messages when the page loads
+//attach to a dom element
+
+
+//initialize post for each page
+//render all of todos from db onto page
