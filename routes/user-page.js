2965482d8e0ebe86c343366caf801097bd616466
James Ivy <jameskivy@gmail.com>
2021-11-04 09:45:37 -0500

    usersPage updates

--- routes/user-page.js
+++ routes/user-page.js
@@ -1,8 +1,17 @@
+//user-page.js in routes
+
 const express = require('express');
 const router = express.Router();
+const db = require('../models')
 
 router.get('/user-page', (req, res) => {
     res.render('user-page')
+})
+
+router.get('/users', async (req, res) => {
+    let users = await db.users.findAll()
+
+    res.json(users)
 })
 
 
