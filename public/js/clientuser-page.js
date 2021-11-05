24e229dab90cd55452a496b75b3756f554b1cce8
James Ivy <jameskivy@gmail.com>
2021-11-05 09:37:07 -0500

    html block

--- public/js/clientuser-page.js
+++ public/js/clientuser-page.js
@@ -6,6 +6,7 @@ let form = document.getElementById('postSubmit')
 
 form.addEventListener('submit', async (e) => {
     e.preventDefault()
+    // console.log('form');
 
     let newPost = {
         title: document.querySelector('#post-title').value,
@@ -42,21 +43,31 @@ const displayStatus = async () => {
 const updateStatus = (postsArr) => {
     let htmlBlock = "";
 
-    postsArr.forEach((element, index) => {
+    postsArr.forEach((element) => {
 
-        //     htmlBlock +=
-        //     htmlBlock +=
-        //     htmlBlock +=
-        //     htmlBlock +=
-        //     htmlBlock +=
+        htmlBlock += '     <div class="">';
+        htmlBlock += '       <div class="">';
+        htmlBlock += '         <div class="">';
+        htmlBlock += '           <div class="">';
+        htmlBlock += '             <div class="post-title">' + element.title + '</div>';
+        htmlBlock += '           </div>';
+        htmlBlock += '          <div>';
+        htmlBlock += '           <div class="post-message">' + element.content + '</div>';
+        htmlBlock += '          </div>';
+        htmlBlock += '         </div>';
+        htmlBlock += '        </div>';
+        htmlBlock += '      </div>';
     })
 
     //attach to a dom element
 
 }
 
+displayStatus()
 
 
 
-//attach to a dom element
+
+
+
 
