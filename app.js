29ca253854830c2c156306bb42d4d23b44abb694
Stephen Doty <stephendoty826@yahoo.com>
2021-11-05 09:44:14 -0500

    committing

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ require("dotenv").config()
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
 
-app.use(express.static(path.join(__dirname, 'public')))
+// app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static('public'))
 app.set('view engine', 'ejs')
 app.use(helmet());
 
