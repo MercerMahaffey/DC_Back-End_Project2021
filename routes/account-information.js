10290a0b26cad6eea8899b2148f8fd0735d9e8b7
MercerMahaffey <89871123+MercerMahaffey@users.noreply.github.com>
2021-11-06 13:21:31 -0400

    account-information page

--- routes/account-information.js
+++ routes/account-information.js
@@ -4,13 +4,34 @@ const router = express.Router();
 const db = require('../models');
 const auth = require('../auth/index');
 
-router.get('/account-information', auth, (req, res) => {
-    res.render('account-information')
+router.get('/account-information', auth, async (req, res) => {
+    let userid = req.session.passport.user;
+    let userRecordsRaw = await db.users.findOne({
+        where: {
+          id: userid 
+        },
+      })
+    let userRecords = userRecordsRaw.dataValues;
+    
+    res.render('account-information', {
+        userRecords
+    })
 })
 router.get('/username', async (req, res) => {
     
     let userRecords = await db.users.findAll()
     res.json(userRecords)
+})
+router.put('/update-account', async (req, res) => {
+    let userid = req.session.passport.user;
+    
+    console.log(userid, req.body);
+    let content = req.body
+
+    
+    await db.users.update(content, {where: {id: userid}});
+    
+    res.json('need this to work')
 })
 
 module.exports = router;
